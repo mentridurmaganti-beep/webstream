4554fc73016df25f8eb85592f0ea80081b647d63
mentridurmaganti-beep <mentridurmaganti@gmail.com>
2026-03-08 19:13:08 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Script from "next/script";
 
-const TELEGRAM_LINK = "https://t.me/+d9TcoaiEqwQ3M2U1";
+const TELEGRAM_LINK = "https://t.me/nontonaja321";
 
 export const metadata = {
   title: "STREAMINGKU - Nonton Film Gratis",
