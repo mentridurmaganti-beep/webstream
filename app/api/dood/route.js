7c2e0609cee9c61ed15097bf93bc90d189f193b0
mentridurmaganti-beep <mentridurmaganti@gmail.com>
2026-03-08 20:35:07 +0700

    Update route.js

--- app/api/dood/route.js
+++ app/api/dood/route.js
@@ -1,10 +1,13 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const API_KEY = "559354bfltkgj0185bxl72"; // API Key kamu
+  // MASUKKAN API KEY BARU KAMU DI SINI
+  const API_KEY = "559354bfltkgj0185bxl72"; 
+  
   try {
-    const response = await fetch(`https://doodapi.com/api/file/list?key=${API_KEY}`);
-    const data = await response.json();
+    const res = await fetch(`https://doodapi.com/api/file/list?key=${API_KEY}`);
+    const data = await res.json();
+    
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ status: 500, msg: error.message });
